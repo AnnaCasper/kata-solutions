f101bdf9a536e5b44532fb931fdc627692d72aa4
Anna Casper <anna.casper3@gmail.com>
2015-08-06 14:40:13 -0600

    new solution

--- solutions.js
+++ solutions.js
@@ -10,3 +10,10 @@ function solution(a, b){
     return a + b + a
   }
 }
+
+// Create a function add(n) which returns a function that always adds n to any number
+function add(n) {
+  return function(x){
+    return x + n
+  }
+}
